283ebb16c84bef6c7f80c45b431878e601659f7f
5ayam5 <sayam.sethi2@gmail.com>
2021-02-24 18:02:29 +0530

    implemented cropping and added print statements

--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@ void mouseHandler(int event, int x, int y, int, void *data)
 		return;
 	vector<Point2f> *corners = (vector<Point2f> *)data;
 	corners->push_back(Point2f(x, y));
-	cout << x << " " << y << "\n";
+	cout << "Clicked at " << x << ", " << y << "\n";
 	if (corners->size() == 4)
-		cout << "Press any key to continue\n";
+		cout << "Press any key to obtain projected image\n";
 }
 
 // rearranges the points in cyclic order starting from the top left and moving clockwise
@@ -35,33 +35,46 @@ Mat computeHomography(vector<Point2f> &corners)
 	return findHomography(corners, findMap(corners));
 }
 
+Rect getRect(vector<Point2f> points)
+{
+	if (points.size() != 4)
+		return Rect();
+	int x = points[0].x, y = points[0].y;
+	return Rect(x, y, int(points[1].x - x), int(points[2].y - y));
+}
+
 int main(int argc, char *argv[])
 {
 	// check if file address present
 	if (argc != 2)
 	{
-		cout << "Required one additional argument:\n./main <file name>\n";
+		cout << "Required one argument:\n./main <file name>\n";
 		return 0;
 	}
 
+	string fileName = argv[1];
+
 	// read the image and check if read correctly
-	Mat img = imread(argv[1], IMREAD_GRAYSCALE);
-	if (img.data == NULL)
+	Mat img = imread(fileName, IMREAD_GRAYSCALE);
+	if (img.data == NULL || fileName.substr(fileName.size() - 3, 3) != "jpg")
 	{
-		cout << "Could not load file, please check the file and try again :(\n";
+		cout << "Could not load file or file not of jpeg format, please check the file and try again :(\n";
 		return 0;
 	}
 
 	Mat perspectiveImg = Mat::zeros(img.rows, img.cols, img.type());
-	string windowName = "Select corners: " + string(argv[1]);
+	string windowName = "Select corners: " + fileName;
 
 	// wait for 4 mouse clicks and store the points in vector "corners"
 	vector<Point2f> corners;
+	cout << "Click on the four corners of the road\n";
 	while (true)
 	{
 		imshow(windowName, img);
 		setMouseCallback(windowName, mouseHandler, &corners);
 		int val = waitKey(0);
+		if (val == 27)
+			return 0;
 		if (corners.size() == 4)
 		{
 			destroyWindow(windowName);
@@ -69,9 +82,19 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	// compute homography matrix from the given corner points and display the resultant image
+	// compute homography matrix from the given corner points and store+display the resultant image
 	Mat h = computeHomography(corners);
 	warpPerspective(img, perspectiveImg, h, Size(img.rows, img.cols));
-	imshow("works?", perspectiveImg);
+	imwrite(fileName.substr(0, fileName.size() - 4) + "Projected.jpg", perspectiveImg);
+	imshow("Projected " + fileName, perspectiveImg);
+	cout << "Press any key to view cropped image\n";
+	waitKey(0);
+	destroyWindow("Projected " + fileName);
+
+	// compute the cropped version of projected image
+	Mat croppedImg = perspectiveImg(getRect(findMap(corners)));
+	imwrite(fileName.substr(0, fileName.size() - 4) + "Cropped.jpg", croppedImg);
+	imshow("Cropped " + fileName, croppedImg);
+	cout << "Press any key to terminate\n";
 	waitKey(0);
 }
