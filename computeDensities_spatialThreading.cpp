ddbb16c1f773ad698bbf7093150ac52e110403a5
5ayam5 <sayam.sethi2@gmail.com>
2021-03-27 17:45:14 +0530

    Improved spatial threading

--- computeDensities_spatialThreading.cpp
+++ computeDensities_spatialThreading.cpp
@@ -6,29 +6,36 @@
 using namespace cv;
 using namespace std;
 
-// segment the frame so that each segment is processed by a different thread
-void segmentFrame(int x, Mat &frame, vector<Mat> &segmentedFrame)
-{
-	int width = frame.size().width, height = frame.size().height / x;
-	for (int i = 0; i < x - 1; ++i)
-		segmentedFrame[i] = frame(Rect(0, i * height, width, height));
-	segmentedFrame.back() = frame(Rect(0, (x - 1) * height, width, frame.size().height - (x - 1) * height));
-}
-
 vector<double> qVals;
 struct ThreadArgs
 {
+	static int NUM_THREADS;
+	static Mat *frame;
 	int thread_id;
-	Mat *frame;
-	Mat *refFrame;
+	Mat refFrame;
 };
+int ThreadArgs::NUM_THREADS;
+Mat *ThreadArgs::frame;
+
+// segment the frame so that each segment is processed by a different thread
+Mat getSegment(Mat &frame, int i)
+{
+	Mat ret;
+	int width = frame.size().width, height = frame.size().height / ThreadArgs::NUM_THREADS;
+	if (i < ThreadArgs::NUM_THREADS - 1)
+		ret = frame(Rect(0, i * height, width, height));
+	else
+		ret = frame(Rect(0, (ThreadArgs::NUM_THREADS - 1) * height, width, frame.size().height - (ThreadArgs::NUM_THREADS - 1) * height));
+	return ret;
+}
 
 // function to compute queue density for each thread
 void *computeQueueThread(void *arg)
 {
 	ThreadArgs *threadArgs = (ThreadArgs *)arg;
 
-	Mat queue = computeQueue(*(threadArgs->refFrame), *(threadArgs->frame));
+	int i = threadArgs->thread_id;
+	Mat frame = getSegment(*(threadArgs->frame), i), queue = computeQueue(threadArgs->refFrame, frame);
 	qVals[threadArgs->thread_id] = sum(queue)[0] * 1.0 / (queue.rows * queue.cols);
 
 	pthread_exit(NULL);
@@ -58,7 +65,7 @@ int main(int argc, char *argv[])
 	double fps = video.get(CAP_PROP_FPS);
 
 	// initialise variables and read the first frame
-	Mat refFrame = getNextFrame(video);
+	Mat refFrame = getNextFrame(video), currFrame;
 	if (refFrame.data == NULL)
 	{
 		cerr << "Error in reading the video file, please check the file and try again.\n";
@@ -72,35 +79,32 @@ int main(int argc, char *argv[])
 	}
 	vector<Point2f> cornersMap = findMap(corners);
 	warpAndCrop(refFrame, corners, cornersMap);
-	int frameNum = 0, x;
+	int frameNum = 0;
 
 	cerr << "Number of threads: ";
-	cin >> x;
+	cin >> ThreadArgs::NUM_THREADS;
 	auto t = chrono::high_resolution_clock::now();
 
-	pthread_t threads[x];
-	vector<Mat> segmentedRefFrame(x), segmentedCurrFrame(x);
-	qVals.resize(x);
-	vector<ThreadArgs> threadArgs(x);
-	for (int i = 0; i < x; ++i)
+	pthread_t threads[ThreadArgs::NUM_THREADS];
+	qVals.resize(ThreadArgs::NUM_THREADS);
+	vector<ThreadArgs> threadArgs(ThreadArgs::NUM_THREADS);
+	for (int i = 0; i < ThreadArgs::NUM_THREADS; ++i)
 	{
-		threadArgs[i].refFrame = &segmentedRefFrame[i];
-		threadArgs[i].frame = &segmentedCurrFrame[i];
+		threadArgs[i].refFrame = getSegment(refFrame, i);
 		threadArgs[i].thread_id = i;
 	}
-	segmentFrame(x, refFrame, segmentedRefFrame);
+	ThreadArgs::frame = &currFrame;
 
 	cout << "time (in seconds),queue density\n";
 
 	while (true)
 	{
-		Mat currFrame = getNextFrame(video);
+		currFrame = getNextFrame(video);
 		if (currFrame.data == NULL)
 			break;
 		warpAndCrop(currFrame, corners, cornersMap);
-		segmentFrame(x, currFrame, segmentedCurrFrame);
 
-		for (int i = 0; i < x; ++i)
+		for (int i = 0; i < ThreadArgs::NUM_THREADS; ++i)
 		{
 			int rc = pthread_create(&threads[i], NULL, computeQueueThread, (void *)&threadArgs[i]);
 			if (rc)
@@ -110,7 +114,7 @@ int main(int argc, char *argv[])
 			}
 		}
 
-		for (int i = 0; i < x; ++i)
+		for (int i = 0; i < ThreadArgs::NUM_THREADS; ++i)
 		{
 			int rc = pthread_join(threads[i], NULL);
 			if (rc)
@@ -120,7 +124,7 @@ int main(int argc, char *argv[])
 			}
 		}
 
-		cout << ++frameNum * 1.0 / fps << ',' << accumulate(qVals.begin(), qVals.end(), (double)0) / x << '\n';
+		cout << ++frameNum * 1.0 / fps << ',' << accumulate(qVals.begin(), qVals.end(), (double)0) / ThreadArgs::NUM_THREADS << '\n';
 	}
 
 	cout << "Time taken: " << chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - t).count() / 1000.0;
